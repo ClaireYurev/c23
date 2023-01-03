2ec48f72733b3e47f379f120c0a164ff53f791ab
Claire Y <61437734+ClaireYurev@users.noreply.github.com>
2023-01-03 10:53:47 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,12 @@
 
 int main() {
   
+  std::cout << "Hello World";
+  // This line is a statement
+    
+  // short for: STANDARD LIBRARY
+  // cout is short for: character out
+  
+  return 0;
+    
 }
